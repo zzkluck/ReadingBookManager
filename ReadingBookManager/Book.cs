20e66c38ea849688a2bd0db74ad72d7c52505624
zzkluck <zzkluck@outlook.com>
2017-02-20 20:26:24 +0800

    添加项目文件。

---
+++ ReadingBookManager/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ReadingBookManager
+{
+	public class Book
+	{
+		private string readPage;
+		private string totalPage;
+		public string BookName { get; set; }
+		public string BookName2 { get; set; }
+		public string Icon { get; set; }
+		public string Position { get; set; }
+		public string RateOfProgress {
+			get
+			{
+				return string.Format("( {0} / {1} ）", readPage, totalPage);
+			}
+		}
+
+		public static Book ReadFromXml(XmlNode BookElement)
+		{
+			if (BookElement.Name != "Book")
+				throw new ArgumentException();
+			Book thisBook = new Book();
+			thisBook.BookName = BookElement.Attributes["Name"].Value;
+			thisBook.BookName2 = BookElement.Attributes["Name2"].Value;
+			thisBook.totalPage = BookElement.Attributes["TotalPage"].Value;
+			thisBook.readPage = BookElement.Attributes["ReadPage"].Value;
+			thisBook.Position = BookElement.Attributes["Position"].Value;
+			return thisBook;
+		}
+	}
+}
---
+++ ReadingBookManager/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace ReadingBookManager
+{
+	/// <summary>
+	/// MainWindow.xaml 的交互逻辑
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		ObservableCollection<Book> MyBooks = new ObservableCollection<Book>();
+		public MainWindow()
+		{
+			InitializeComponent();
+			Main.DataContext = MyBooks;
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			string path = @"C:\Users\admin\Desktop\ReadingBooks.xml";
+			XmlDocument xd = new XmlDocument();
+			xd.Load(path);
+			XmlElement rootNode = xd.DocumentElement;
+			XmlNodeList Books = rootNode.GetElementsByTagName("Book");
+
+			foreach (XmlNode node in Books)
+			{
+				Book book = Book.ReadFromXml(node);
+				MyBooks.Add(book);
+			}
+			
+		}
+
+	}
+}
