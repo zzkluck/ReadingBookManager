8309bfc4baff4a6e9c8d6807f4e299e2f2b89a6d
zzkluck <zzkluck@outlook.com>
2017-04-03 19:27:47 +0800

    填了一大堆奇怪的东西

---
+++ ReadingBookManager/BookListView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReadingBookManager
+{
+    /// <summary>
+    /// BookListView.xaml 的交互逻辑
+    /// </summary>
+    public partial class BookListView : Page
+    {
+        public BookListView()
+        {
+            InitializeComponent();
+            LstVwReadingRecords.DataContext = MainWindow.MyBookManager.ReadingRecordsList;
+        }
+        private void ButtonHeader_Click(object sender, RoutedEventArgs e)
+        {
+            GridViewColumn gvc = (e.OriginalSource as GridViewColumnHeader).Column;
+            switch (gvc.Header.ToString())
+            {
+                case "BookName":
+                    LstVwReadingRecords.DataContext =
+                MainWindow.MyBookManager.ReadingRecordsList.OrderBy((ReadingRecordByPage r) => r.BookName);
+                    break;
+                case "Page":
+                    LstVwReadingRecords.DataContext =
+                MainWindow.MyBookManager.ReadingRecordsList.OrderBy((ReadingRecordByPage r) => r.ReadPage);
+                    break;
+                case "Date":
+                    LstVwReadingRecords.DataContext =
+                MainWindow.MyBookManager.ReadingRecordsList.OrderBy((ReadingRecordByPage r) => r.Date);
+                    break;
+                default:
+                    throw new Exception("Somethins error");
+            }
+            }
+        }
+
+
+}
--- ReadingBookManager/MainWindow.xaml.cs
+++ ReadingBookManager/MainWindow.xaml.cs
@@ -23,13 +23,12 @@ namespace ReadingBookManager
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		BookManager MyBookManager = new BookManager();
-		public MainWindow()
+		public static BookManager MyBookManager = new BookManager();
+        int i = 0;
+        public MainWindow()
 		{
 			InitializeComponent();
-			LstBxBooks.DataContext = MyBookManager.ReadingBooks;
-			LstVwReadingRecords.DataContext = MyBookManager.ReadingRecordsList;
-			StateBar.DataContext = MyBookManager;
+			StatusRateOfRead.DataContext = MyBookManager;
 			SplashScreen ss = new SplashScreen("Loading.png");
 			ss.Show(true);
 		}
@@ -39,36 +38,13 @@ namespace ReadingBookManager
 			MyBookManager.GetReadingRecordByPage();
 		}
 
-		private void ButtonHeader_Click(object sender, RoutedEventArgs e)
-		{
-            GridViewColumn gvc = (e.OriginalSource as GridViewColumnHeader).Column;
-			switch (gvc.Header.ToString())
-			{
-				case "BookName":
-					LstVwReadingRecords.DataContext =
-				MyBookManager.ReadingRecordsList.OrderBy((ReadingRecordByPage r) => r.BookName);
-					break;
-				case "Page":
-					LstVwReadingRecords.DataContext =
-				MyBookManager.ReadingRecordsList.OrderBy((ReadingRecordByPage r) => r.ReadPage);
-					break;
-				case "Date":
-					LstVwReadingRecords.DataContext =
-				MyBookManager.ReadingRecordsList.OrderBy((ReadingRecordByPage r) => r.Date);
-					break;
-				default:
-					throw new Exception("Somethins error");
-			}
-		}
-
 		private void Window_KeyDown(object sender, KeyEventArgs e)
 		{
-			if (e.Key == Key.Enter)
-			{
-				LstBxBooks.Visibility = (LstBxBooks.Visibility == Visibility) ? Visibility.Collapsed : Visibility.Visible;
-				LstVwReadingRecords.Visibility = (LstVwReadingRecords.Visibility == Visibility.Visible) ? Visibility.Collapsed : Visibility.Visible;
-			}
-		}
+            if (i++ % 2 == 0)
+                FrameMain.Navigate(new BookListView());
+            else
+                FrameMain.Navigate(new BookListBox());
+        }
     }
 }
 
