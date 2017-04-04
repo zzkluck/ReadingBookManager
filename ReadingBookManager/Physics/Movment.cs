9b702c91bfd7c121ade23d3dafd440df2d1af946
zzkluck <zzkluck@outlook.com>
2017-04-04 23:05:33 +0800

    完成了一部分绘图，还有很大缺陷，然而停电了2333

--- ReadingBookManager/Physics/Movment.cs
+++ ReadingBookManager/Physics/Movment.cs
@@ -8,16 +8,39 @@ namespace ReadingBookManager.Physics
 {
     public class MovmentCanon
     {
-        static double g = 9.8;
-        double angelAlpha, angelGamma;
-        double InitalizingVelocity;
-        double InitalizingHeight;
-        double CononLength;
-        private Position DisplacementEquation(double timeSpan)
+        public static double g = 9.8;
+        public double AngelAlpha, AngelGamma;
+        public double InitalizingVelocity;
+        public double InitalizingHeight;
+        public double CononLength;
+
+        private double cosX, cosY, cosZ;
+
+        public MovmentCanon
+            (double angelAlpha,double angelGamma,double cononLength,double initalizingVelocity,double initalizingHeight)
+        {
+            AngelAlpha = angelAlpha;
+            AngelGamma = angelGamma;
+            CononLength = cononLength;
+            InitalizingHeight = initalizingHeight;
+            InitalizingVelocity = initalizingVelocity;
+            cosX = - Math.Sin(angelAlpha) * Math.Cos(angelGamma);
+            cosY=Math.Cos(angelAlpha);
+            cosZ = Math.Sin(angelAlpha) * Math.Sin(angelGamma);
+        }
+
+        public MovmentCanon
+    (string angelAlpha, string angelGamma, string cononLength, string initalizingVelocity, string initalizingHeight)
+            :this(Convert.ToDouble(angelAlpha), Convert.ToDouble(angelGamma), Convert.ToDouble(cononLength), Convert.ToDouble(initalizingVelocity), Convert.ToDouble(initalizingHeight))
+        { }
+        public Position DisplacementEquation(double timeSpan)
         {
             double v0 = InitalizingVelocity;
             double y0 = InitalizingHeight;
-            throw new NotImplementedException();
+            return new Position(
+                v0 * cosX * timeSpan,
+                y0 + CononLength * cosY + v0 * cosY * timeSpan - 0.5 * g * timeSpan * timeSpan,
+                v0 * cosZ * timeSpan);
         }
     }
 
--- ReadingBookManager/Physics/PhysicsSimulation.xaml.cs
+++ ReadingBookManager/Physics/PhysicsSimulation.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ReadingBookManager.Physics;
 
 namespace ReadingBookManager
 {
@@ -23,6 +24,43 @@ namespace ReadingBookManager
         public PhysicsSimulation()
         {
             InitializeComponent();
+        }
+
+        private Line buildDefaultLine(double x1,double y1,double x2,double y2)
+        {
+            Line newLine = new Line();
+            newLine.X1 = x1;
+            newLine.X2 = x2;
+            newLine.Y1 = y1;
+            newLine.Y2 = y2;
+            newLine.Stroke = Brushes.Black;
+            newLine.StrokeThickness = 2;
+            return newLine;
+        }
+        private void addLineToCanvas(Line l,Canvas c)
+        {
+            if(l.Y2>c.Height||l.X2>c.Width||l.X2<0||l.Y2<0)
+            {
+                throw new NotImplementedException();
+            }
+            c.Children.Add(l);
+        }
+        private void FireButton_Click(object sender, RoutedEventArgs e)
+        {
+            MovmentCanon Canon = new MovmentCanon
+                (AngelAlphaTextBox.Text, AngelGammaTextBox.Text, CononLengthTextBox.Text, InitalizingVelocityTextBox.Text, CononHeightTextBox.Text);
+            int i = 1000;
+            double t = 0;
+            Position previous = Canon.DisplacementEquation(0);
+            while(i-->0)
+            {
+                Position now = Canon.DisplacementEquation(t);
+                t += 0.01;
+                
+                addLineToCanvas(buildDefaultLine(previous.Item1, previous.Item3+200, now.Item1, now.Item3+200), TopViewCanvas);
+                addLineToCanvas(buildDefaultLine(previous.Item1, previous.Item2, now.Item1, now.Item2), SideViewCanvas);
+                previous = now;
+            }
         }
     }
 }
