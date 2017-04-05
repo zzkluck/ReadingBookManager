6edc8daaa262a5a4ef881c6ab37f395d1e23c514
zzkluck <zzkluck@outlook.com>
2017-04-05 14:25:34 +0800

    勉强算能用了

--- ReadingBookManager/Physics/Movment.cs
+++ ReadingBookManager/Physics/Movment.cs
@@ -9,23 +9,101 @@ namespace ReadingBookManager.Physics
     public class MovmentCanon
     {
         public static double g = 9.8;
-        public double AngelAlpha, AngelGamma;
-        public double InitalizingVelocity;
-        public double InitalizingHeight;
-        public double CononLength;
 
-        private double cosX, cosY, cosZ;
-
-        public MovmentCanon
-            (double angleAlpha,double angleGamma,double cononLength,double initalizingVelocity,double initalizingHeight)
+        #region field
+        private double angleAlpha;
+        private double angleGamma;
+        private double initalizingVelocity;
+        private double initalizingHeight;
+        private double cononLength;
+        public double cosX, cosY, cosZ;
+        #endregion
+        #region Property
+        public double AngleAlpha
         {
-            AngelAlpha = angleAlpha;
-            AngelGamma = angleGamma;
+            get
+            {
+                return angleAlpha;
+            }
+
+            set
+            {
+                if (value < 0 || value > 180)
+                    throw new ArgumentOutOfRangeException("AngleAlpha", "角度数应在0~180之间");
+                angleAlpha = value;
+            }
+        }
+
+        public double AngleGamma
+        {
+            get
+            {
+                return angleGamma;
+            }
+
+            set
+            {
+                if (value < -90 || value > 90)
+                    throw new ArgumentOutOfRangeException("AngleGamma", "角度数应在-90~0之间");
+                angleGamma = value;
+            }
+        }
+
+        public double InitalizingVelocity
+        {
+            get
+            {
+                return initalizingVelocity;
+            }
+
+            set
+            {
+                if (value < 0 || value > 76000)
+                    throw new ArgumentOutOfRangeException("InitalizingVelocity", "速度值应当大于0且不应过大");
+                initalizingVelocity = value;
+            }
+        }
+
+        public double InitalizingHeight
+        {
+            get
+            {
+                return initalizingHeight;
+            }
+
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("InitalizingHeight");
+                initalizingHeight = value;
+            }
+        }
+
+        public double CononLength
+        {
+            get
+            {
+                return cononLength;
+            }
+
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("CononLength");
+                cononLength = value;
+            }
+        }
+        #endregion
+        public MovmentCanon
+            (double angleAlpha, double angleGamma, double cononLength, double initalizingVelocity, double initalizingHeight)
+        {
+            AngleAlpha = angleAlpha;
+            AngleGamma = angleGamma;
             CononLength = cononLength;
             InitalizingHeight = initalizingHeight;
             InitalizingVelocity = initalizingVelocity;
-            cosX =  Math.Sin(angleToRadin(angleAlpha)) * Math.Cos(angleToRadin(angleGamma));
-            cosY=Math.Cos(angleToRadin(angleAlpha));
+            cosX = Math.Sin(angleToRadin(angleAlpha)) * Math.Cos(angleToRadin(angleGamma));
+            cosY = Math.Cos(angleToRadin(angleAlpha));
             cosZ = Math.Sin(angleToRadin(angleAlpha)) * Math.Sin(angleToRadin(angleGamma));
         }
         private static double angleToRadin(double angleValue)
@@ -35,21 +113,23 @@ namespace ReadingBookManager.Physics
 
         public MovmentCanon
     (string angelAlpha, string angelGamma, string cononLength, string initalizingVelocity, string initalizingHeight)
-            :this(Convert.ToDouble(angelAlpha), Convert.ToDouble(angelGamma), Convert.ToDouble(cononLength), Convert.ToDouble(initalizingVelocity), Convert.ToDouble(initalizingHeight))
+            : this(Convert.ToDouble(angelAlpha), Convert.ToDouble(angelGamma), Convert.ToDouble(cononLength), Convert.ToDouble(initalizingVelocity), Convert.ToDouble(initalizingHeight))
         { }
         public Position DisplacementEquation(double timeSpan)
         {
             double v0 = InitalizingVelocity;
-            double y0 = InitalizingHeight;
+            double x0 = CononLength * cosX;
+            double y0 = InitalizingHeight + CononLength * cosY;
+            double z0 = CononLength * cosZ;
             return new Position(
-                v0 * cosX * timeSpan,
-                y0 + CononLength * cosY + v0 * cosY * timeSpan - 0.5 * g * timeSpan * timeSpan,
-                v0 * cosZ * timeSpan);
+                x0 + v0 * cosX * timeSpan,
+                y0 + v0 * cosY * timeSpan - 0.5 * g * timeSpan * timeSpan,
+                z0 + v0 * cosZ * timeSpan);
         }
     }
 
     public class Position : Tuple<double, double, double>
     {
-        public Position(double x,double y,double z) : base(x, y, z) { }
+        public Position(double x, double y, double z) : base(x, y, z) { }
     }
 }
--- ReadingBookManager/Physics/PhysicsSimulation.xaml.cs
+++ ReadingBookManager/Physics/PhysicsSimulation.xaml.cs
@@ -29,33 +29,43 @@ namespace ReadingBookManager
         private void FireButton_Click(object sender, RoutedEventArgs e)
         {
             //TODO: 学习线程相关的知识，制作绘制线的动画
-            MovmentCanon Canon = new MovmentCanon
-                (AngelAlphaTextBox.Text, AngelGammaTextBox.Text, CononLengthTextBox.Text, InitalizingVelocityTextBox.Text, CononHeightTextBox.Text);
-            double t = 0;
-            Position initialPosition = Canon.DisplacementEquation(0);
-            StringBuilder pathDescriptionSide = new StringBuilder();
-            pathDescriptionSide.AppendFormat("M {0},{1} L ", initialPosition.Item1, SideViewCanvas.ActualHeight-initialPosition.Item2);
-
-            while (true)
+            try
             {
-                Position nowPosition = Canon.DisplacementEquation(t);
-                if (getStatus(nowPosition) != Status.Drawing)
-                    break;
-                t += 0.1;
-                pathDescriptionSide.AppendFormat("{0},{1} ", nowPosition.Item1, SideViewCanvas.ActualHeight - nowPosition.Item2);
-            }
-            Path path = new Path();
-            path.Data = Geometry.Parse(pathDescriptionSide.ToString());           
-            path.Style = (Style)Resources["flaringPathStyle"];
-            SideViewCanvas.Children.Add(path);
-        }
+                MovmentCanon Canon = new MovmentCanon
+                (AngelAlphaTextBox.Text, AngelGammaTextBox.Text, CononLengthTextBox.Text, InitalizingVelocityTextBox.Text, CononHeightTextBox.Text);
+                DrawConon(Canon);
+                double t = 0;
+                Position initialPosition = Canon.DisplacementEquation(0);
 
-        private Status getStatus(Position p)
-        {
-            if (p.Item2 > SideViewCanvas.ActualHeight || p.Item1 > SideViewCanvas.ActualWidth || p.Item1 < 0 || p.Item2 < 0)
-                return Status.CrossTheBorder;
-            else
-                return Status.Drawing;
+                StringBuilder pathDescriptionTop = new StringBuilder();
+                pathDescriptionTop.AppendFormat("M {0},{1} L ", initialPosition.Item1, CanvasYConvert(initialPosition.Item3 + SideViewCanvas.ActualHeight / 2, SideViewCanvas));
+                StringBuilder pathDescriptionSide = new StringBuilder();
+                pathDescriptionSide.AppendFormat("M {0},{1} L ", initialPosition.Item1, CanvasYConvert(initialPosition.Item2, SideViewCanvas));
+                
+                Status TopDrawStatus = Status.Drawing, SideDrawStatus = Status.Drawing;
+                while (TopDrawStatus == Status.Drawing || SideDrawStatus == Status.Drawing)
+                {
+                    t += 0.1;
+                    Position nowPosition = Canon.DisplacementEquation(t);
+                    if ((TopDrawStatus = getStatus(nowPosition, TopOrSide.Top)) == Status.Drawing)
+                        pathDescriptionTop.AppendFormat("{0},{1} ", nowPosition.Item1, CanvasYConvert(nowPosition.Item3 + SideViewCanvas.ActualHeight / 2, TopViewCanvas));
+                    if ((SideDrawStatus = getStatus(nowPosition, TopOrSide.Side)) == Status.Drawing)
+                        pathDescriptionSide.AppendFormat("{0},{1} ", nowPosition.Item1, CanvasYConvert(nowPosition.Item2,SideViewCanvas));
+                }
+                Path path = new Path();
+                path.Data = Geometry.Parse(pathDescriptionTop.ToString());
+                path.Style = (Style)Resources["flaringPathStyle"];
+                TopViewCanvas.Children.Add(path);
+                Path path2 = new Path();
+                path2.Data = Geometry.Parse(pathDescriptionSide.ToString());
+                path2.Style = (Style)Resources["flaringPathStyle"];
+                SideViewCanvas.Children.Add(path2);
+            }
+            catch (ArgumentOutOfRangeException excp)
+            {
+                MessageBox.Show(excp.Message);
+            }
+
         }
 
         private void ReFreshButton_Click(object sender, RoutedEventArgs e)
@@ -63,12 +73,53 @@ namespace ReadingBookManager
             TopViewCanvas.Children.Clear();
             SideViewCanvas.Children.Clear();
         }
+        private void DrawConon(MovmentCanon c)
+        {
+            Path cononTop = new Path();
+            cononTop.Stroke = Brushes.Brown;
+            cononTop.StrokeThickness = 5;
+            cononTop.Data = Geometry.Parse
+                (string.Format("M {0},{1} L{2},{3}", 0, TopViewCanvas.ActualHeight / 2, c.CononLength * c.cosX, CanvasYConvert(c.CononLength * c.cosZ + TopViewCanvas.ActualHeight / 2, TopViewCanvas)));
+            Path cononSide = new Path();
+            cononSide.Stroke = Brushes.Brown;
+            cononSide.StrokeThickness = 5;
+            cononSide.Data = Geometry.Parse
+                (string.Format("M {0},{1} L{2},{3}", 0, CanvasYConvert(c.InitalizingHeight, SideViewCanvas), c.CononLength * c.cosX, CanvasYConvert(c.CononLength * c.cosY + c.InitalizingHeight, TopViewCanvas)));
+            TopViewCanvas.Children.Add(cononTop);
+            SideViewCanvas.Children.Add(cononSide);
+        }
+        private double CanvasYConvert(double YValue, Canvas c)
+        {
+            return c.ActualHeight - YValue;
+        }
+        private Status getStatus(Position p, TopOrSide tos)
+        {
+            switch (tos)
+            {
+                case TopOrSide.Top:
+                    if (p.Item3 > TopViewCanvas.ActualHeight/2 || p.Item1 > TopViewCanvas.ActualWidth || p.Item1 < 0 || p.Item3 < -TopViewCanvas.ActualHeight / 2)
+                        return Status.CrossTheBorder;
+                    else
+                        return Status.Drawing;
+                case TopOrSide.Side:
+                    if (p.Item2 > SideViewCanvas.ActualHeight || p.Item1 > SideViewCanvas.ActualWidth || p.Item1 < 0 || p.Item2 < 0)
+                        return Status.CrossTheBorder;
+                    else
+                        return Status.Drawing;
+                default:
+                    throw new ArgumentException();
+            }
 
+        }
         enum Status
         {
             Drawing,
             Collision,
             CrossTheBorder
+        }
+        enum TopOrSide
+        {
+            Top, Side
         }
     }
 }
